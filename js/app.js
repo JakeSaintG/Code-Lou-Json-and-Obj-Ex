117efef2850959257be26596cea38eaddc7766a2
JakeSaintG <jakestgermain@outlook.com>
2022-06-24 01:48:14 -0400

    prettier go brrrrr

--- js/app.js
+++ js/app.js
@@ -2,32 +2,32 @@
 document.getElementById("nav-hamburger").checked = false;
 let navOpen = false;
 
-document.getElementById("nav-hamburger").addEventListener('click', () => {
-    if (navOpen === false) {
-        document.querySelector("nav").style.animationName = "slide-down";
-        document.querySelector("nav").style.display = "flex";
-        navOpen = true;
-    } else {
-        document.querySelector("nav").style.animationName = "slide-up";
-        setTimeout(() => {
-            document.querySelector("nav").style.display = "none";
-        }, 250); // Enough delay to clear the animation. 
-        navOpen = false;
-    }
-})
+document.getElementById("nav-hamburger").addEventListener("click", () => {
+  if (navOpen === false) {
+    document.querySelector("nav").style.animationName = "slide-down";
+    document.querySelector("nav").style.display = "flex";
+    navOpen = true;
+  } else {
+    document.querySelector("nav").style.animationName = "slide-up";
+    setTimeout(() => {
+      document.querySelector("nav").style.display = "none";
+    }, 250); // Enough delay to clear the animation.
+    navOpen = false;
+  }
+});
 
 function toonsHandler() {
-    fetch('./json/toon.json')
-        .then(r => r.json())
-        .then(data => printToons(data));
+  fetch("./json/toon.json")
+    .then((r) => r.json())
+    .then((data) => printToons(data));
 }
 
 printToons = (data) => {
-    // console.log(data);
-    data.cartoons.forEach(e => {
-        newDiv = document.createElement('div');
-        newDiv.classList.add('tile');
-        newDiv.innerHTML = `
+  console.log(data);
+  data.cartoons.forEach((e) => {
+    newDiv = document.createElement("div");
+    newDiv.classList.add("tile");
+    newDiv.innerHTML = `
         <div class='rating ${e.rating}'>${e.rating}</div>
         <div class='bio'>
             <img height="90" width="90" src="${e.img}" alt="Img of ${e.name}" class="toon-img">
@@ -38,12 +38,19 @@ printToons = (data) => {
             </div>
         </div>
         `;
-        document.getElementById('cartoon-tiles').appendChild(newDiv);
-        // console.log(e.name);
-    });
-    
-    // Also works:
-    // for (const employee of data.cartoons) {
-    //     console.log(employee.name);
-    // };
-}
+    document.getElementById("cartoon-tiles").appendChild(newDiv);
+    // console.log(e.name);
+  });
+
+  // Also works:
+  // for (const employee of data.cartoons) {
+  //     console.log(employee.name);
+  // };
+};
+
+
+let test = "breakpoint?"
+test = "break"
+test = "break--point????"
+test = "point"
+test = "breakpoint!"
